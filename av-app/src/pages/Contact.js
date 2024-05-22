54d33ddf66a0e598b34a87bbc6787125a0d6a8c7
alexdennis2001 <alejandrodennis2001@outlook.com>
2024-05-22 14:10:00 -0600

    Started working on contact page

--- av-app/src/pages/Contact.js
+++ av-app/src/pages/Contact.js
@@ -1,7 +1,28 @@
+import { Container, Stack  } from 'react-bootstrap';
+import Header from '../components/Header/Header';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Contact() {
     return (
-      <p>Contact page</p>
+      <div>
+      <Header/>
+      <Container>
+      <Row className="justify-content-md-center">
+        <Col xs lg="2" sm={8}>
+          <h2>Get In Touch With Us!</h2>
+          <Stack gap={3}>
+            <div className="p-2">First item</div>
+            <div className="p-2">Second item</div>
+            <div className="p-2">Third item</div>
+          </Stack>
+        </Col>
+        <Col xs lg="2" sm={8}>
+          3 of 3
+        </Col>
+      </Row>
+      </Container>
+    </div>
     );
   }
   
