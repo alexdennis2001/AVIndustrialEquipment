43e1452e61898ec9af4a47efe35427f52aa1b567
Alejandro Dennis <Alejandro.Dennis@OmniSource.com>
2024-06-26 10:17:48 -0600

    Correct API url

--- av-app/src/api/axiosConfig.js
+++ av-app/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: 'https://localhost:44378/',
+  baseURL: 'http://137.184.3.233/',
   headers: {
     'Content-Type': 'application/json',
     // Any other common headers
